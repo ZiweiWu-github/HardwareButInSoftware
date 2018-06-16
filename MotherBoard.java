160de51f5d5584d4ed8bd352517454a8ea005dd0
Ziwei Wu <wu8982008@yahoo.com>
2018-06-16 11:11:26 -0700

    Added multiplication and shift "circuits"
    multiplication logic is here along with helper logic stuff
    there's now a 1-bit full adder and half-adder
    shift left and right are here, now with shifts with desired numbers too

--- MotherBoard.java
+++ MotherBoard.java
@@ -6,9 +6,10 @@ Has addition and subtraction "circuits" implemented
 public class MotherBoard {
 
 	public static void main(String[] args) {
-		int[] s = decimalToBinary(-2,10);
-		int[] f = decimalToBinary(10,10);
-		int[] g = addOrSub(s,f, 1);
+		int[] s = decimalToBinary(2,4);
+		int[] f = decimalToBinary(2,4);
+		int[] g = addOrSub(s, f,1);
+		s = ReduceRight(s);
 		printOut(g);
 		System.out.println(binaryToDecimal(g));
 	}
@@ -28,13 +29,40 @@ public class MotherBoard {
 		if(a==1) return 0;
 		else return 1;
 	}
+	public static int[] halfAdder(int a, int b) {
+		int[] s = new int[2];
+		//s[0] is the sum, s[1] is the carry bit
+		s[0] = logicalXor1Bit(a,b);
+		s[1] = logicalAnd1Bit(a,b);
+		return s;
+	}
+	public static int[] fullAdder1Bit(int a, int b, int c) {
+		int[] s = new int[2];
+		//s[0] is the sum, s[1] is the carry bit
+		int[] h = halfAdder(a,b);
+		s[0] = logicalXor1Bit(c, h[0]);
+		s[1] = logicalOr1Bit(h[1], logicalAnd1Bit(c, logicalXor1Bit(a, b)));
+		return s;
+	}
 	public static int[] fullAdder(int[] a, int[] b, int m) {
 		int c = m;
 		int[] s = new int[a.length];
 		for(int i =0; i<a.length; i++) {
-			s[i] = logicalXor1Bit(c, logicalXor1Bit(a[i],b[i]));
-			c = logicalOr1Bit(logicalAnd1Bit(a[i], b[i]), logicalAnd1Bit(c, logicalXor1Bit(a[i], b[i])));
+			int[] r = fullAdder1Bit(a[i], b[i], c);
+			s[i] = r[0];
+			c = r[1];
 		}
+		return s;
+	}
+	public static int[] fullAdderWithCarry(int[] a, int[] b, int m) {
+		int c = m;
+		int[] s = new int[a.length+1];
+		for(int i =0; i<a.length; i++) {
+			int[] r = fullAdder1Bit(a[i], b[i], c);
+			s[i] = r[0];
+			c = r[1];
+		}
+		s[s.length-1] = c;
 		return s;
 	}
 	public static int[] addOrSub(int[] a, int[] b, int mode) {
@@ -48,6 +76,64 @@ public class MotherBoard {
 			}
 			return fullAdder(a,f,mode);
 		}
+	}
+	public static int[] shiftRight(int[] a) {
+		int[] s = new int[a.length];
+		for(int i =0; i<a.length-1; i++) {
+			s[i] = a[i+1];
+		}
+		s[a.length-1] = 0;
+		return s;
+	}
+	public static int[] shiftLeft(int[] a) {
+		int[] s = new int[a.length];
+		for(int i =1; i<a.length; i++) {
+			s[i] = a[i-1];
+		}
+		s[0] = 0;
+		return s;
+	}
+	public static int[] shiftRightWithNum(int[] a, int num) {
+		int[] s = shiftRight(a);
+		s[a.length-1] = num;
+		return s;
+	}
+	public static int[] shiftLeftWithNum(int[] a, int num) {
+		int[] s = shiftLeft(a);
+		s[0] = num;
+		return s;
+	}
+	public static int[] ReduceRight(int[]a) {
+		//gets rid of the LSB 
+		//kinda like shift right, but the size of the array is shrunk by 1
+		int[] s = new int[a.length-1];
+		for(int i =1; i<a.length; i++) {
+			s[i-1] = a[i]; 
+		}
+		return s;
+	}
+	public static int[] oneToManyAnds(int[]a, int b) {
+		int[] s = new int[a.length];
+		for(int i =0; i<a.length; i++) {
+			s[i] = logicalAnd1Bit(a[i], b);
+		}
+		return s;
+	}
+	public static int[] multiplier(int[] a, int[] b) {
+		int[] s = new int[a.length + b.length];
+		int[] tempHold = oneToManyAnds(a, b[0]);
+		s[0] = tempHold[0];
+		tempHold = shiftRight(tempHold);
+		for(int i =1; i<a.length; i++) {
+			int[] bAndA = oneToManyAnds(a, b[i]);
+			tempHold = fullAdderWithCarry(tempHold, bAndA, 0);
+			s[i] = tempHold[0];
+			tempHold = ReduceRight(tempHold);
+		}
+		for(int i = 0; i<tempHold.length; i++) {
+			s[s.length-i-1] = tempHold[tempHold.length-i-1];
+		}
+		return s;
 	}
 	public static int[] decimalToBinary(int aa, int size) {
 		int[] s = new int[size];
